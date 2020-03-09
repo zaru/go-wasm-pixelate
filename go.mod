35280eb7af0efc990b38e572c301113d6952a25b
zaru <sakuraba@basicinc.jp>
2020-03-09 13:13:39 +0900

    temp commit

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/zaru/wasm-sample
 go 1.13
 
 require (
-	github.com/lucasb-eyer/go-colorful v1.0.2
-	github.com/muesli/clusters v0.0.0-20180605185049-a07a36e67d36
+	github.com/lucasb-eyer/go-colorful v1.0.3
+	github.com/muesli/clusters v0.0.0-20190807044042-ba9c57dd9228
 	github.com/muesli/kmeans v0.0.0-20190917235210-80dfc71e6c5a
+	golang.org/x/image v0.0.0-20200119044424-58c23975cae1 // indirect
 )
--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/muesli/clusters"
-	"github.com/muesli/kmeans"
-	"syscall/js"
 	"github.com/lucasb-eyer/go-colorful"
+	"github.com/muesli/clusters"
+	"syscall/js"
 )
 
 func colorAvg(data []float64) float64 {
@@ -23,7 +22,7 @@ func pixelate(cc clusters.Clusters, colorIndexes map[string][]int) {
 
 	width := 400
 	height := 400
-	grid := 10
+	grid := 20
 
 	for _, c := range cc {
 		for _, org := range c.Observations {
@@ -56,67 +55,104 @@ func convertLab(hex string) (float64, float64, float64) {
 	return l, a, b
 }
 
+
 func main() {
-	var d clusters.Observations
+	c := make(chan struct{})
+	var draw js.Func
 
 	canvas := js.Global().Get("document").Call("getElementById", "canvas")
 	ctx := canvas.Call("getContext", "2d")
+
 	width := 400
 	height := 400
-	grid := 10
+	grid := 20
 	total := grid * grid
 
-	var colorIndexes = map[string][]int{}
+	draw = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		//var d clusters.Observations
 
-	for x := 0; x < width / grid; x++ {
-		for y := 0; y < height / grid; y++ {
-			// 分割したセルの中の色平均を計算
-			cell := ctx.Call("getImageData", x*grid, y*grid, grid, grid)
-			data := cell.Get("data")
-			var rs = make([]float64, total)
-			var gs = make([]float64, total)
-			var bs = make([]float64, total)
-			for p := 0; p < total; p++ {
-				r := data.Index(p*4).Int()
-				g := data.Index(p*4+1).Int()
-				b := data.Index(p*4+2).Int()
-				labL, labA, labB := convertLab(fmt.Sprintf("#%x%x%x", r, g, b))
-				rs[p] = labL
-				gs[p] = labA
-				bs[p] = labB
+		//var colorIndexes = map[string][]int{}
+
+		for x := 0; x < width / grid; x++ {
+			for y := 0; y < height / grid; y++ {
+
+				// 分割したセルの中の色平均を計算
+				cell := ctx.Call("getImageData", x*grid, y*grid, grid, grid)
+				data := cell.Get("data")
+				uint8Arr := js.Global().Get("Uint8Array").New(data)
+				received := make([]byte, data.Get("length").Int())
+				_ = js.CopyBytesToGo(received, uint8Arr)
+				//r := data.Index(100).Int()
+				//r2 := received[100]
+				//fmt.Printf("r = %x\n", r)
+				//fmt.Printf("r2 = %x\n", r2)
+
+				var rs = make([]float64, total)
+				var gs = make([]float64, total)
+				var bs = make([]float64, total)
+				for p := 0; p < total; p++ {
+					//r := data.Index(p*4).Int()
+					//g := data.Index(p*4+1).Int()
+					//b := data.Index(p*4+2).Int()
+					//_ = data.Index(p)
+					r := received[p*4]
+					g := received[p*4+1]
+					b := received[p*4+2]
+					//fmt.Sprintf("#%x%x%x", r, g, b)
+					//_ = data.Index(p*4+1).Int()
+					//_ = data.Index(p*4+2).Int()
+					//labL, labA, labB := convertLab(fmt.Sprintf("#%x%x%x", r, g, b))
+					//rs[p] = labL
+					//gs[p] = labA
+					//bs[p] = labB
+					rs[p] = r
+					gs[p] = g
+					bs[p] = b
+				}
+				//ravg := colorAvg(rs)
+				//gavg := colorAvg(gs)
+				//bavg := colorAvg(bs)
+				_ = colorAvg(rs)
+				_ = colorAvg(gs)
+				_ = colorAvg(bs)
+
+				////c := colorful.Lab(ravg, gavg, bavg)
+				////hex := c.Hex()
+				////r, _ := strconv.ParseUint(hex[1:2], 16, 0)
+				////g, _ := strconv.ParseUint(hex[3:4], 16, 0)
+				////b, _ := strconv.ParseUint(hex[5:6], 16, 0)
+				////key := fmt.Sprintf("%d%d%d", r, g, b)
+				////fmt.Printf("%s\n", key)
+				//
+				//// 最後に色を置換する際に計算量を減らすためのメモ
+				////key := fmt.Sprintf("%d%d%d", ravg, gavg, bavg)
+				//key := fmt.Sprintf("%d%d%d", ravg, gavg, bavg)
+				//index := x * (width / grid) + y
+				//colorIndexes[key] = append(colorIndexes[key], index)
+				//
+				//d = append(d, clusters.Coordinates{
+				//	float64(ravg),
+				//	float64(gavg),
+				//	float64(bavg),
+				//})
 			}
-			ravg := colorAvg(rs)
-			gavg := colorAvg(gs)
-			bavg := colorAvg(bs)
-
-			//c := colorful.Lab(ravg, gavg, bavg)
-			//hex := c.Hex()
-			//r, _ := strconv.ParseUint(hex[1:2], 16, 0)
-			//g, _ := strconv.ParseUint(hex[3:4], 16, 0)
-			//b, _ := strconv.ParseUint(hex[5:6], 16, 0)
-			//key := fmt.Sprintf("%d%d%d", r, g, b)
-			//fmt.Printf("%s\n", key)
-
-			// 最後に色を置換する際に計算量を減らすためのメモ
-			//key := fmt.Sprintf("%d%d%d", ravg, gavg, bavg)
-			key := fmt.Sprintf("%d%d%d", ravg, gavg, bavg)
-			index := x * (width / grid) + y
-			colorIndexes[key] = append(colorIndexes[key], index)
-
-			d = append(d, clusters.Coordinates{
-				float64(ravg),
-				float64(gavg),
-				float64(bavg),
-			})
 		}
-	}
 
-	km := kmeans.New()
-	cc, err := km.Partition(d, 8)
+		//km := kmeans.New()
+		//_, err := km.Partition(d, 8)
 
-	if err != nil {
-		panic(err)
-	}
+		//if err != nil {
+		//	panic(err)
+		//}
 
-	pixelate(cc, colorIndexes)
+		//pixelate(cc, colorIndexes)
+
+		fmt.Println("a")
+		js.Global().Call("requestAnimationFrame", draw)
+		return nil
+	})
+	defer draw.Release()
+
+	js.Global().Call("requestAnimationFrame", draw)
+	<-c
 }
